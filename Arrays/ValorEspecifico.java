0f0d4684aa3fb348e5d165b8f2705b5865ac4a1c
Kelenvs <kelenvs@gmail.com>
2025-09-30 23:45:28 -0300

    Subindo  arquivos

---
+++ Arrays/ValorEspecifico.java
@@ -0,0 +1,13 @@
+public class ValorEspecifico {
+    public static void main(String[]args){
+
+    int[] numeros = {85, 65, 92, 55, 78, 90, 45};
+    int cont = 0;
+    for(int i = 0; i < numeros.length; i++) { 
+    if(numeros[i] >= 70){
+        cont++;
+    }
+    } 
+    System.out.println("Existem " + cont + " notas maior que 70." );
+}
+}
---
+++ Arrays/temperatura.java
@@ -0,0 +1,14 @@
+public class temperatura {
+    public static void main(String[]args){
+        int[] temperaturas = {23, 27, 31, 28, 25, 30, 26};
+        int maior = temperaturas[0];
+ 
+        for(int i = 0; i < temperaturas.length; i++) { 
+     
+        if(temperaturas[i] > maior){
+          maior = temperaturas[i];
+          }
+       } 
+        System.out.println("A maior temperatura " + maior );
+    }
+}
