392a3739c8331300fa2adea9900352b9d09fcc33
Kelenvs <kelenvs@gmail.com>
2025-10-19 01:24:22 -0300

    novos exercicios

---
+++ NumerosPares.java
@@ -0,0 +1,16 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class NumerosPares {
+     public static void main(String[] args) throws IOException {
+        int cont = 0;
+        int[] numeros = {2, 7, -4, 6, -5, 12};
+            for(int i = 0; i < numeros.length; i++){
+                int numero = numeros[i];
+            if(numero % 2 == 0){
+               cont++;
+            }
+        }
+        System.out.println(cont + " valores pares");
+    }
+}
