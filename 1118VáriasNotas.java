c1e998f977ad8c0d5b1bece5ebc335d82beae89c
Kelenvs <kelenvs@gmail.com>
2025-12-04 17:29:19 -0300

    exercicios adicionados

---
+++ 1118VáriasNotas.java
@@ -0,0 +1,39 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class 1118VáriasNotas {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            double nota1 = lerNotaValida(scanner);
+            double nota2 = lerNotaValida(scanner);
+            double media = (nota1 + nota2) / 2;
+            System.out.printf("media = %.2f\n", media);
+
+            int resultado;
+            do {
+                System.out.println("novo calculo (1-sim 2-nao)");
+                resultado = scanner.nextInt();
+            } while (resultado != 1 && resultado != 2);
+
+            if (resultado == 2) {
+                break;
+            }
+        }
+
+        scanner.close();
+    }
+
+    public static double lerNotaValida(Scanner scanner) {
+        double nota;
+        while (true) {
+            nota = scanner.nextDouble();
+            if (nota >= 0 && nota <= 10) {
+                return nota;
+            } else {
+                System.out.println("nota invalida");
+            }
+        }
+    }
+}
